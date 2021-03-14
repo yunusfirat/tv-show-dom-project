b2ba81a6364b2ff30d0b9e78a9ae714b42f370cb
yunusfirat <yunusfirat44@gmail.com>
2021-03-14 17:20:59 +0000

    added grid

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  episodeList.forEach(episode => {
+  let h1 = document.createElement('h1');
+  let div = document.createElement('div');
+  let img = document.createElement('img');
+  let p = document.createElement('p');
+  img.src = `${episode.image.medium}`
+  div.classList.add('card');
+  div.appendChild(h1);
+  div.appendChild(img);
+  div.appendChild(p);
+  h1.innerHTML = `${episode.name}`;
+  p.innerHTML = `${episode.summary}`
+  rootElem.appendChild(div);
+  
 
+  })
+}
 window.onload = setup;
