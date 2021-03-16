5b616a0cd23264787cd405b21aec3c66cc4986ce
yunusfirat <yunusfirat44@gmail.com>
2021-03-16 01:20:46 +0000

    counting displayed movies

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+  console.log(episodeList.length);
   episodeList.forEach(episode => {
     let h1 = document.createElement('h1');
     let div = document.createElement('div');
@@ -24,18 +25,25 @@ function makePageForEpisodes(episodeList) {
   let input = document.querySelector('#moviesearch')
   input.addEventListener('keyup', function () {
     let cards = document.querySelectorAll(".card");
+    let count = 0;
     let filter = input.value.toUpperCase();
     cards.forEach(card => {
       let h1Value = card.childNodes[0].textContent || card.childNodes[0].innerHTML
       let pValue = card.childNodes[2].textContent || card.childNodes[2].innerHTML;
+      let foundmovies = document.getElementById('moviescount');
       if (h1Value.toUpperCase().indexOf(filter) > -1 || pValue.toUpperCase().indexOf(filter) > -1) {
+        card.style.display = "";
       } else {
         card.style.display = "none";
       }
+      let check = card.style.display === "none"
+      if(check === false){
+        count = count + 1;
+      }
+      foundmovies.innerHTML = `Displaying ${count}/${rootElem.children.length} movies`
     })
   
   })
-
 }
 
 
